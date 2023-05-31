3840b0421fe917029ad3dd46e862ff9ab74d5207
yakubujosepheleojojoel@gmail.com <yakubujosepheleojojoel@gmail.com>
2023-05-31 16:16:07 +0100

    Tv mobile

--- src/Home/Navigations/NavTv.js
+++ src/Home/Navigations/NavTv.js
@@ -21,6 +21,7 @@ function NavTv() {
       const headerColor = {
         color: 'white',
         overFlowY: 'hidden',
+        textAlign:'center'
       };
       const imageStyle = {
         width: '100%',
@@ -31,14 +32,14 @@ function NavTv() {
     <div>
       <div>
         <Nav />
-        <div className='page-container'>
-            <h1>Television</h1>
+        <div className='presenterPage-container'>
+          <h1>Television</h1>
           <div className='gridcontainer'>
             <div className='newsGrid'>
               {data.slice(0, visible).map((item) => {
                 return (
                   <div>
-                    <div className='frameImage'></div>
+                    {/* <div className='frameImage'></div> */}
                     <Link to={`/Himma-Tv/${item.id}`} key={item.id}>
                       <div className='newsCardImage'>
                         <iframe
@@ -53,6 +54,44 @@ function NavTv() {
                         {/* <img src={item.image} alt='' style={imageStyle} /> */}
                       </div>
                       <div className='newsCardHeader'>
+                        <h3 style={headerColor} className='textLimit'>
+                          {item.title}
+                        </h3>
+                      </div>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className='moreBtn'>
+            <button className='viewmore' onClick={showMoreItems}>
+              View More
+            </button>
+          </div>
+        </div>
+        <div className='page-container-mobile'>
+          <h1>Television</h1>
+          <div className='gridcontainer'>
+            <div className='newsGrid-shows'>
+              {data.slice(0, visible).map((item) => {
+                return (
+                  <div className='tv-card-mobile' >
+                    {/* <div className='frameImage'></div> */}
+                    <Link to={`/Himma-Tv/${item.id}`} key={item.id}>
+                      <div className='newsCardImage_mobile'>
+                        <iframe
+                          width='100%'
+                          height='100%'
+                          src='https://www.youtube.com/embed/WOgUgbui-sk'
+                          title='YouTube video player'
+                          frameborder='0'
+                          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                          allowfullscreen
+                        ></iframe>
+                        {/* <img src={item.image} alt='' style={imageStyle} /> */}
+                      </div>
+                      <div className='profileInfo'>
                         <h3 style={headerColor} className='textLimit'>
                           {item.title}
                         </h3>
