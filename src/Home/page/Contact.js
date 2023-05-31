3ac7ff1e7e1fc40695e3f95b2013640c42f884b5
yakubujosepheleojojoel@gmail.com <yakubujosepheleojojoel@gmail.com>
2023-05-31 23:54:24 +0100

    changed

--- src/Home/page/Contact.js
+++ src/Home/page/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Nav from '../Nav';
 
 function Contact (){
   return (
     <div>
+      <Nav/>
       <div className="contact-container">
         <div className="contact">
+          <h1>Contact Us</h1>
           
         </div>
       </div>
