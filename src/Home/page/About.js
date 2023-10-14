0236bb9aed9a89c9593d6971d470f6f034c65fe5
yakubujosepheleojojoel@gmail.com <yakubujosepheleojojoel@gmail.com>
2023-10-14 14:03:32 +0100

    Changed about us

--- src/Home/page/About.js
+++ src/Home/page/About.js
@@ -29,28 +29,49 @@ Whether you are a farmer, researcher, or simply passionate about agriculture, ou
         <h1 className='headersFont'>About Us</h1>
         <p className='about-content'>
           <p style={{ fontSize: '16px', lineHeight: '1.5' }}>
-            HIMMA Radio and Television by{' '}
-            <a
-              href='#'
-              onClick={handleClick}
-              style={{
-                border: 'none',
-                color: 'blue',
-                fontWeight: 'bold',
-              }}
-            >
-              Sarman Integrated Farms Limited
-            </a>{' '}
-            is dedicated to further extension and propagation of agricultural
-            practices within its locality. <br /> Whether you are a farmer,
-            researcher, or simply passionate about agriculture, our human-touch
-            radio service will keep you informed, inspired, and connected to the
-            pulse of the agricultural community.
-            <br /> <h1 className='headersFont'>Our Vision</h1> To be the most
-            Credible, Consistent and Valued Agricultural Broadcast Station in
-            Nigeria. <br /> <h1 className='headersFont'>Our Mission</h1> To
-            provide a trusted and reliable source of information, education and
-            entertainment to the communities we serve.
+            HIMMA Radio and Television by IKRA MULTIMEDIA LIMITED is dedicated
+            to further extension and propagation of agricultural practices
+            within its locality, Nigeria and beyond. <br />
+            <br />
+            Broadcasting from the ancient commercial city of Kano State, we are
+            programmed with modern and contemporary agricultural content and
+            IKRA MULTIMEDIA LIMITED is the only Agricultural and Farming
+            formatted Radio and Television station in the area and does not
+            solicit funds from listeners. <br />
+            <br />
+            We are Committed to providing the Kano urban and rural with the best
+            in Agricultural Programmes. We offer modern and contemporary Farming
+            techniques tailored towards meeting the needs of our listeners and
+            viewers. <br />
+            <br />
+            Following us at Himma Radio and Television promises our teeming
+            audience a very rich mix of Agricultural shows, local news, weather
+            reports, commodity prices, sports, entertainments, giveaways and
+            lots more. <br />
+            <br />
+            Are you a farmer, researcher, or simply passionate about
+            agriculture, our human-touch radio and television services will keep
+            you informed, inspired, and connected to the pulse of the
+            agricultural community at home and abroad! <br />
+            <h1 className='headersFont'>Our Vision</h1>
+            To ensure sustainable food systems in Nigeria and Africa through
+            promoting Agribusiness. <br />
+            <h1 className='headersFont'>Our Mission</h1>
+            To promote commercial farming and agricultural entrepreneurship
+            through capacity development and exchange of quality information and
+            farmer-to-farmer training programs in local languages, thereby
+            reaching the lost and encouraging the saved through our Agricultural
+            and Farming formats. <br />
+            <h1 className='headersFont'>Our Core Values</h1>
+            <ul>
+              <li>Respect and value for local knowledge and innovations</li>
+              <li>Value for the environment and ecosystems</li>
+              <li>Professional, effective, and informed by relevant science</li>
+              <li>Creative, flexible, and innovative</li>
+              <li>Sensitive to gender</li>
+              <li>Quality, equality, and ethics</li>
+              <li>Passion. </li>
+            </ul>
           </p>
         </p>
         <div>
